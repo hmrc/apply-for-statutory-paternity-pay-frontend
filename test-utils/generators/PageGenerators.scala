7c0bff8989841605c69db3fbc7d72f325e0615ad
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2022-06-09 11:36:47 +0100

    Add NINO page

--- test-utils/generators/PageGenerators.scala
+++ test-utils/generators/PageGenerators.scala
@@ -21,6 +21,9 @@ import pages._
 
 trait PageGenerators {
 
+  implicit lazy val arbitraryNinoPage: Arbitrary[NinoPage.type] =
+    Arbitrary(NinoPage)
+
   implicit lazy val arbitraryWillTakeTimeToSupportMotherPage: Arbitrary[WillTakeTimeToSupportMotherPage.type] =
     Arbitrary(WillTakeTimeToSupportMotherPage)
 
--- test-utils/generators/UserAnswersEntryGenerators.scala
+++ test-utils/generators/UserAnswersEntryGenerators.scala
@@ -24,6 +24,14 @@ import play.api.libs.json.{JsValue, Json}
 
 trait UserAnswersEntryGenerators extends PageGenerators with ModelGenerators {
 
+  implicit lazy val arbitraryNinoUserAnswersEntry: Arbitrary[(NinoPage.type, JsValue)] =
+    Arbitrary {
+      for {
+        page  <- arbitrary[NinoPage.type]
+        value <- arbitrary[String].suchThat(_.nonEmpty).map(Json.toJson(_))
+      } yield (page, value)
+    }
+
   implicit lazy val arbitraryWillTakeTimeToSupportMotherUserAnswersEntry: Arbitrary[(WillTakeTimeToSupportMotherPage.type, JsValue)] =
     Arbitrary {
       for {
