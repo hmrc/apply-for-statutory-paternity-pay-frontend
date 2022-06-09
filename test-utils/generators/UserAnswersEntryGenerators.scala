c94193391d2eb49202e39e72ac00431c41fb6f6e
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2022-06-09 10:46:19 +0100

    Add missing page and update content

--- test-utils/generators/UserAnswersEntryGenerators.scala
+++ test-utils/generators/UserAnswersEntryGenerators.scala
@@ -24,6 +24,14 @@ import play.api.libs.json.{JsValue, Json}
 
 trait UserAnswersEntryGenerators extends PageGenerators with ModelGenerators {
 
+  implicit lazy val arbitraryWantPayToStartOnDueDateUserAnswersEntry: Arbitrary[(WantPayToStartOnDueDatePage.type, JsValue)] =
+    Arbitrary {
+      for {
+        page  <- arbitrary[WantPayToStartOnDueDatePage.type]
+        value <- arbitrary[Boolean].map(Json.toJson(_))
+      } yield (page, value)
+    }
+
   implicit lazy val arbitraryWillTakeTimeToSupportMotherUserAnswersEntry: Arbitrary[(WillTakeTimeToSupportMotherPage.type, JsValue)] =
     Arbitrary {
       for {
--- test-utils/generators/UserAnswersGenerator.scala
+++ test-utils/generators/UserAnswersGenerator.scala
@@ -27,6 +27,7 @@ trait UserAnswersGenerator extends TryValues {
   self: Generators =>
 
   val generators: Seq[Gen[(QuestionPage[_], JsValue)]] =
+    arbitrary[(WantPayToStartOnDueDatePage.type, JsValue)] ::
     arbitrary[(WillTakeTimeToSupportMotherPage.type, JsValue)] ::
     arbitrary[(WillTakeTimeToCareForChildPage.type, JsValue)] ::
     arbitrary[(WillHaveCaringResponsibilityPage.type, JsValue)] ::
