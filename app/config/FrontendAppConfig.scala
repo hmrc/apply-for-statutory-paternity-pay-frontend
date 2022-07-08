75820c40bdf8d6d2a48c9ead5e815991a326fbf1
Chris Wright <chrisjameswright@users.noreply.github.com>
2022-07-08 11:43:56 +0100

    Fixing beta feedback URL

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -29,7 +29,7 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   val appName: String = configuration.get[String]("appName")
 
   private val contactHost = configuration.get[String]("contact-frontend.host")
-  private val contactFormServiceIdentifier = "apply-for-statutory-paternity-pay-frontend"
+  private val contactFormServiceIdentifier = "apply-for-statutory-paternity-pay"
 
   def feedbackUrl(implicit request: RequestHeader): String =
     s"$contactHost/contact/beta-feedback?service=$contactFormServiceIdentifier&backUrl=${SafeRedirectUrl(host + request.uri).encodedUrl}"
