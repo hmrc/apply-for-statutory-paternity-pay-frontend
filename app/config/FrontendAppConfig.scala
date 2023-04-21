6b5090cd83681bd1393bdf054abd9e0a49137b58
Michael Wolfendale <4563722+wolfendale@users.noreply.github.com>
2023-04-21 11:16:05 +0100

    Fix exit survey url

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -38,8 +38,7 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   val loginContinueUrl: String = configuration.get[String]("urls.loginContinue")
   val signOutUrl: String       = configuration.get[String]("urls.signOut")
 
-  private val exitSurveyBaseUrl: String = configuration.get[Service]("microservice.services.feedback-frontend").baseUrl
-  val exitSurveyUrl: String             = s"$exitSurveyBaseUrl/feedback/apply-for-statutory-paternity-pay-frontend"
+  val exitSurveyUrl: String = s"$contactHost/feedback/apply-for-statutory-paternity-pay-frontend"
 
   val languageTranslationEnabled: Boolean =
     configuration.get[Boolean]("features.welsh-translation")
