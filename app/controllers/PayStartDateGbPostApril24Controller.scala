f47004fdb317f8bbb3b59c66f42c158024f8ea53
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2024-02-28 17:17:44 +0000

    Update guidance on Pay Start Date Post April 24 page

--- app/controllers/PayStartDateGbPostApril24Controller.scala
+++ app/controllers/PayStartDateGbPostApril24Controller.scala
@@ -41,33 +41,42 @@ class PayStartDateGbPostApril24Controller @Inject()(
                                                      formProvider: PayStartDateGbPostApril24FormProvider,
                                                      val controllerComponents: MessagesControllerComponents,
                                                      view: PayStartDateGbPostApril24View
-                                      )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+                                      )(implicit ec: ExecutionContext)
+  extends FrontendBaseController
+    with I18nSupport
+    with AnswerExtractor {
 
   def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) {
     implicit request =>
-      val form = formProvider()
+      getPaternityReason { paternityReason =>
 
-      val preparedForm = request.userAnswers.get(PayStartDateGbPostApril24Page) match {
-        case None => form
-        case Some(value) => form.fill(value)
+        val form = formProvider()
+
+        val preparedForm = request.userAnswers.get(PayStartDateGbPostApril24Page) match {
+          case None => form
+          case Some(value) => form.fill(value)
+        }
+
+        Ok(view(preparedForm, mode, paternityReason))
       }
-
-      Ok(view(preparedForm, mode))
   }
 
   def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async {
     implicit request =>
-      val form = formProvider()
+      getPaternityReasonAsync { paternityReason =>
 
-      form.bindFromRequest().fold(
-        formWithErrors =>
-          Future.successful(BadRequest(view(formWithErrors, mode))),
+        val form = formProvider()
 
-        value =>
-          for {
-            updatedAnswers <- Future.fromTry(request.userAnswers.set(PayStartDateGbPostApril24Page, value))
-            _              <- sessionRepository.set(updatedAnswers)
-          } yield Redirect(navigator.nextPage(PayStartDateGbPostApril24Page, mode, updatedAnswers))
-      )
+        form.bindFromRequest().fold(
+          formWithErrors =>
+            Future.successful(BadRequest(view(formWithErrors, mode, paternityReason))),
+
+          value =>
+            for {
+              updatedAnswers <- Future.fromTry(request.userAnswers.set(PayStartDateGbPostApril24Page, value))
+              _              <- sessionRepository.set(updatedAnswers)
+            } yield Redirect(navigator.nextPage(PayStartDateGbPostApril24Page, mode, updatedAnswers))
+        )
+    }
   }
 }
--- test/controllers/PayStartDateGbPostApril24ControllerSpec.scala
+++ test/controllers/PayStartDateGbPostApril24ControllerSpec.scala
@@ -17,15 +17,15 @@
 package controllers
 
 import java.time.{LocalDate, ZoneOffset}
-
 import base.SpecBase
 import forms.PayStartDateGbPostApril24FormProvider
-import models.{NormalMode, UserAnswers}
+import models.{NormalMode, PaternityReason, UserAnswers}
 import navigation.{FakeNavigator, Navigator}
+import org.eclipse.jetty.util.resource.PathResource
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
 import org.scalatestplus.mockito.MockitoSugar
-import pages.PayStartDateGbPostApril24Page
+import pages.{IsAdoptingOrParentalOrderPage, PayStartDateGbPostApril24Page}
 import play.api.inject.bind
 import play.api.mvc.{AnyContentAsEmpty, AnyContentAsFormUrlEncoded, Call}
 import play.api.test.FakeRequest
@@ -50,7 +50,7 @@ class PayStartDateGbPostApril24ControllerSpec extends SpecBase with MockitoSugar
 
   lazy val payStartDateGbPostApril24Route = routes.PayStartDateGbPostApril24Controller.onPageLoad(NormalMode).url
 
-  override val emptyUserAnswers = UserAnswers(userAnswersId)
+  val answers = emptyUserAnswers.set(IsAdoptingOrParentalOrderPage, false).success.value
 
   def getRequest(): FakeRequest[AnyContentAsEmpty.type] =
     FakeRequest(GET, payStartDateGbPostApril24Route)
@@ -67,7 +67,7 @@ class PayStartDateGbPostApril24ControllerSpec extends SpecBase with MockitoSugar
 
     "must return OK and the correct view for a GET" in {
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      val application = applicationBuilder(userAnswers = Some(answers)).build()
 
       running(application) {
         val result = route(application, getRequest).value
@@ -75,13 +75,13 @@ class PayStartDateGbPostApril24ControllerSpec extends SpecBase with MockitoSugar
         val view = application.injector.instanceOf[PayStartDateGbPostApril24View]
 
         status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form, NormalMode)(getRequest, messages(application)).toString
+        contentAsString(result) mustEqual view(form, NormalMode, PaternityReason.PaternityFromBirth)(getRequest, messages(application)).toString
       }
     }
 
     "must populate the view correctly on a GET when the question has previously been answered" in {
 
-      val userAnswers = UserAnswers(userAnswersId).set(PayStartDateGbPostApril24Page, validAnswer).success.value
+      val userAnswers = answers.set(PayStartDateGbPostApril24Page, validAnswer).success.value
 
       val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
 
@@ -91,7 +91,7 @@ class PayStartDateGbPostApril24ControllerSpec extends SpecBase with MockitoSugar
         val result = route(application, getRequest).value
 
         status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form.fill(validAnswer), NormalMode)(getRequest, messages(application)).toString
+        contentAsString(result) mustEqual view(form.fill(validAnswer), NormalMode, PaternityReason.PaternityFromBirth)(getRequest, messages(application)).toString
       }
     }
 
@@ -102,7 +102,7 @@ class PayStartDateGbPostApril24ControllerSpec extends SpecBase with MockitoSugar
       when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
 
       val application =
-        applicationBuilder(userAnswers = Some(emptyUserAnswers))
+        applicationBuilder(userAnswers = Some(answers))
           .overrides(
             bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
             bind[SessionRepository].toInstance(mockSessionRepository)
@@ -119,7 +119,7 @@ class PayStartDateGbPostApril24ControllerSpec extends SpecBase with MockitoSugar
 
     "must return a Bad Request and errors when invalid data is submitted" in {
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      val application = applicationBuilder(userAnswers = Some(answers)).build()
 
       val request =
         FakeRequest(POST, payStartDateGbPostApril24Route)
@@ -133,7 +133,7 @@ class PayStartDateGbPostApril24ControllerSpec extends SpecBase with MockitoSugar
         val result = route(application, request).value
 
         status(result) mustEqual BAD_REQUEST
-        contentAsString(result) mustEqual view(boundForm, NormalMode)(request, messages(application)).toString
+        contentAsString(result) mustEqual view(boundForm, NormalMode, PaternityReason.PaternityFromBirth)(request, messages(application)).toString
       }
     }
 
